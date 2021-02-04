cf35b1e4fc0ddb0430eb1e285ce1fc1849a28a82
fergussmyth <FergusSmyth05@gmail.com>
2021-02-04 13:05:58 +0000

    Final commit, full functionality prints images and current status.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Hangman
-Simple Hangman game!
+
+- Simple hangman game!
+
+![Hangman](https://github.com/fergussmyth/Hangman/blob/main/HANGMAN.kpg)
+
+
+
+
+
+
+
+
+
+
+__*Credit to Sanjin Dedic*__ https://www.youtube.com/channel/UCAh1jhlX0WZtY6GNxPTv02Q
+
--- main.py
+++ main.py
@@ -1,32 +1,62 @@
-import random
+import os, random, time, assets
 
 f = open('word.txt', 'r')
-
-word = 'HAPPINESS'
+word = random.choice(f.readlines()).strip().upper()
 reveal = list(len(word)*'_')
 lives = 7
-gameWon = False
+game_won = False
+
+print('Welcome to HANGMAN!')
+time.sleep(1)
+print('Game is starting..')
+time.sleep(2)
 
 
-while gameWon == False and lives > 0:
-    print(reveal)
+def letter_check(letter, word):
+    """
+    Function to check if single letter is in word, if so replaces blank with letter.
+    :param letter:
+    :param word:
+    :return:
+    """
+    global reveal
+    global game_won
+    global lives
+    for i in range(0, len(word)):
+        letter = word[i]
+        if guess == letter:
+            reveal[i] = guess
+    if '_' not in reveal:
+        game_won = True
+    else:
+        game_won = False
+
+
+def status():
+    """
+    Current status of lives remaining, changes assets picture.
+    :return:
+    """
+    os.system('cls')
+    print(assets.pictures[7-lives])
+    print(' '.join([str(e) for e in reveal]))
+    print('You have', lives, 'lives remaining!')
+
+
+while game_won == False and lives > 0:
+    status()
     guess = input('Guess a letter or word: ')
     guess = guess.upper()
-
     if guess == word:
-        gameWon = True
+        game_won = True
     if len(guess) == 1 and guess in word:
-        for i in range(0,len(word)):
-            letter = word[i]
-            if guess == letter:
-                reveal[i] = guess
-        if '_' not in reveal:
-            gameWon = True
+        letter_check(guess, word)
     else:
         lives -= 1
 
 
-if gameWon:
-    print('Well done you won!')
+if game_won:
+    print('Well done you won! The final word was', word)
 else:
     print('You failed! The word was', word)
+
